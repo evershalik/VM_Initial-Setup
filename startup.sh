ec2865f1d84f5826b30a9e0e13b850504293715a
SHANKAR <98207888+sha-lik@users.noreply.github.com>
2022-06-17 01:15:58 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -40,7 +40,26 @@ EOF
 kubeadm init --config config.yaml
 
 # install docker
-sudo apt-get install docker.io
+# sudo apt-get install docker.io
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg-agent \
+  software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+     $(lsb_release -cs) \
+     stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+
+
 sudo apt-get install docker-compose
 
 # apt-mark hold is used so that these packages will not be updated/removed automatically
